02ecb33000b3b2d2970d200ae44310a097c60695
wenhaibo <1461154748@qq.com>
2020-08-03 17:48:43 +0800

    提交视频录制

---
+++ sample/src/main/java/com/mabeijianxi/smallvideo/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.mabeijianxi.smallvideo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mabeijianxi.camera.MediaRecorderActivity;
+
+public class MainActivity2 extends MediaRecorderActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        
+
+
+    }
+}
